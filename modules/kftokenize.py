5aa9b703843896369ab3999759ce594e85bd60b4
Jazgalor <Jazgalor@wp.pl>
2023-12-01 16:05:52 +0100

    kftokenize.py - copied number regex

--- modules/kftokenize.py
+++ modules/kftokenize.py
@@ -2,6 +2,7 @@
 """
 
 import collections
+import re
 from kftoken import *
 
 import kftoken
@@ -13,3 +14,24 @@ class TokenNode(collections.namedtuple('TokenNode', 'type string start end line'
         annotated_type = '%d (%s)' % (self.type, names[self.type])
         return ('TokenInfo(type=%s, string=%r, start=%r, end=%r, line=%r)' %
                 self._replace(type=annotated_type))
+
+# regex group functions (grouping with or)
+def group(*choices): return '(' + '|'.join(choices) + ')'
+def any(*choices): return group(*choices) + '*'
+def maybe(*choices): return group(*choices) + '?'
+
+# making regex for every number literal
+Hexnumber = r'0[xX](?:_?[0-9a-fA-F])+'
+Binnumber = r'0[bB](?:_?[01])+'
+Octnumber = r'0[oO](?:_?[0-7])+'
+Decnumber = r'(?:0(?:_?0)*|[1-9](?:_?[0-9])*)'
+Intnumber = group(Hexnumber, Binnumber, Octnumber, Decnumber)
+Exponent = r'[eE][-+]?[0-9](?:_?[0-9])*'
+Pointfloat = group(r'[0-9](?:_?[0-9])*\.(?:[0-9](?:_?[0-9])*)?', r'\.[0-9](?:_?[0-9])*') + maybe(Exponent)
+Expfloat = r'[0-9](?:_?[0-9])*' + Exponent
+Floatnumber = group(Pointfloat, Expfloat)
+Imagnumber = group(r'[0-9](?:_?[0-9])*[jJ]', Floatnumber + r'[jJ]')
+# combine all number regexes into single regex
+Number = group(Imagnumber, Floatnumber, Intnumber)
+
+
