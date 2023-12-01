abd9c4cb00bb18264ec6b48bcf88dbdd99cbfed0
Jazgalor <Jazgalor@wp.pl>
2023-12-01 13:05:19 +0100

    kftoken.py added space

--- modules/kftoken.py
+++ modules/kftoken.py
@@ -67,4 +67,5 @@ NT_OFFSET = 256
 tok_name = {value: name
             for name, value in globals().items()
             if isinstance(value, int) and not name.startswith('_')}
+
 __all__.extend(tok_name.values())
