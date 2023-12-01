417dd21affc22f4dcdae25ef8378fe1ec752bd62
Jazgalor <Jazgalor@wp.pl>
2023-12-01 12:47:07 +0100

    running some tests

---
+++ modules/test.py
@@ -0,0 +1,10 @@
+import tokenize as tkn
+import token as tk
+import kftoken
+import re
+from token import EXACT_TOKEN_TYPES
+
+def group(*choices): return '(' + '|'.join(choices) + ')'
+
+print(group(*map(re.escape, sorted(EXACT_TOKEN_TYPES, reverse=True))))
+print(kftoken.tok_name)
