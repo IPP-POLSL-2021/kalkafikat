6bff1cfb62ad840c03e121a09574e80d994922a6
Jazgalor <Jazgalor@wp.pl>
2023-12-02 18:21:39 +0100

    test.py - test with encodings

--- modules/test.py
+++ modules/test.py
@@ -2,6 +2,7 @@ import tokenize as tkn
 import token as tk
 import kftoken
 import re
+import codecs
 from token import EXACT_TOKEN_TYPES
 
 def group(*choices): return '(' + '|'.join(choices) + ')'
@@ -10,3 +11,7 @@ print(group(*map(re.escape, sorted(EXACT_TOKEN_TYPES, reverse=True))))
 print(kftoken.names)
 print(EXACT_TOKEN_TYPES.keys())
 print(tkn.Funny)
+
+f = open('tokens.txt', 'rb')
+b = f.readline()
+print(b.startswith(codecs.BOM_UTF8))
