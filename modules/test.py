37e27c32eb3c9696f7baecafb22e75d562997dd9
w0jd <dwoitek1@gmail.com>
2024-01-24 19:51:22 +0100

    wywoływanie z pliku osobnego
    Erwin teraz możesz dodać swój kod w końcu aby był interface

--- modules/test.py
+++ modules/test.py
@@ -5,6 +5,8 @@ import re
 from io import TextIOWrapper
 import codecs
 from token import EXACT_TOKEN_TYPES
+import glob
+import numpy as np
 
 
 def similar(str1, str2):
@@ -14,29 +16,34 @@ def similar(str1, str2):
                for i, j in zip(str1, str2)) / float(len(str1))
 
 
-with kftokenize.open('./kftokenize.py') as f:
-    original_tokens = list(kftokenize.tokenize(f.readline))
+def TakPlagiat(str1):
+    plagiatNaTaken = 0
+    with kftokenize.open(str1) as f:
+        original_tokens = list(kftokenize.tokenize(f.readline))
 
-    pliki = glob.glob('./baza/*.py')
-    print(pliki)
-    tabavg = []
+        pliki = glob.glob('./baza/*.py')
+        print(pliki)
+        tabavg = []
 
-    for token in original_tokens:
-        max_similarity_per_line = []
+        for token in original_tokens:
+            max_similarity_per_line = []
 
-        for plik in pliki:
-            with kftokenize.open(plik) as plagiat:
-                plagiat_tokens = list(kftokenize.tokenize(plagiat.readline))
-                max_similarity = 0
+            for plik in pliki:
+                with kftokenize.open(plik) as plagiat:
+                    plagiat_tokens = list(
+                        kftokenize.tokenize(plagiat.readline))
+                    max_similarity = 0
 
-                for token2 in plagiat_tokens:
-                    tok_str2 = repr(token2)
-                    token_str = repr(token)
-                    max_similarity = max(
-                        max_similarity, similar(token_str, tok_str2))
+                    for token2 in plagiat_tokens:
+                        tok_str2 = repr(token2)
+                        token_str = repr(token)
+                        max_similarity = max(
+                            max_similarity, similar(token_str, tok_str2))
 
-                max_similarity_per_line.append(max_similarity)
+                    max_similarity_per_line.append(max_similarity)
 
-        tabavg.append(max(max_similarity_per_line))
+            tabavg.append(max(max_similarity_per_line))
 
-    print(np.average(tabavg) * 100)
+        print(np.average(tabavg) * 100)
+        plagiatNaTaken = (np.average(tabavg) * 100)
+    return plagiatNaTaken
